fd5c1cb2a7da5dd0e88e92b38ae42a505a5dae29
ata barış canalp <atabariscanalp@outlook.com>
2021-06-04 14:03:19 +0300

    -configured middleware

--- berlin/src/index.ts
+++ berlin/src/index.ts
@@ -4,9 +4,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { ProfileResolver, UserResolver } from './resolvers';
 import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth20';
 import { PrismaClient } from '@prisma/client';
-import { randomInt } from 'crypto';
 import middleware from './middleware';
 import dayjs from 'dayjs';
 
@@ -15,58 +13,12 @@ import dayjs from 'dayjs';
   const app = express();
   const prisma = new PrismaClient();
 
-  passport.use(
-    new GoogleStrategy.Strategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL: 'http://localhost:5000/auth/google/callback',
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        const { id, displayName, emails, photos } = profile;
-        const user = await prisma.user.findUnique({
-          where: {
-            googleId: id,
-          },
-        });
-        console.log('USER ', user);
-        if (!user) {
-          const newUser = await prisma.user.create({
-            data: {
-              username: displayName
-                ? displayName
-                : `user-${randomInt(321983798, 9484239098, (err, n) => {
-                    if (err) return '47584803';
-                  })}`,
-              email: emails![0].value,
-              googleId: id,
-              profile: {
-                create: {
-                  avatar: photos![0].value,
-                },
-              },
-            },
-          });
-          return done(null, newUser, {
-            access_token: accessToken,
-            refresh_token: refreshToken,
-          });
-        }
-        console.log(profile);
-        return done(null, user, {
-          access_token: accessToken,
-          refresh_token: refreshToken,
-        });
-      }
-    )
-  );
-
   middleware(app);
 
   app.get(
-    '/google-login',
+    '/auth/google',
     passport.authenticate('google', {
-      scope: ['email'],
+      scope: ['email', 'profile'],
       accessType: 'offline',
     })
   );
@@ -81,7 +33,7 @@ import dayjs from 'dayjs';
         httpOnly: true,
         expires: dayjs().add(30, 'days').toDate(),
       });
-      res.send('AUTH IS GOOOOD');
+      return res.redirect('http://localhost:3000/home');
     })(req, res, next);
   });
 
--- berlin/src/middleware/index.ts
+++ berlin/src/middleware/index.ts
@@ -2,8 +2,10 @@ import { Application } from 'express';
 import passport from 'passport';
 import requestMethods from './requestMethods';
 import cookieParser from 'cookie-parser';
+import { Strategy } from './passport.config';
 
 export default (app: Application) => {
+  passport.use(Strategy);
   app.use(requestMethods);
   app.use(passport.initialize());
   app.use(cookieParser());
