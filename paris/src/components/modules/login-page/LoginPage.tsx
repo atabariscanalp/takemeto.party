a98d5e5cc6801e9af618ef224e2e4a21d2c3b819
atabariscanalp <atabariscanalp@outlook.com>
2021-06-05 23:00:51 +0300

    -added docker-compose and .env.example

--- paris/src/components/modules/login-page/LoginPage.tsx
+++ paris/src/components/modules/login-page/LoginPage.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Button } from '../../ui/Button'
-import GoogleIcon from '../../../icons'
-
+import React from "react";
+import { Button } from "../../ui/Button";
+import GoogleIcon from "../../../icons";
 
 export const LoginPage: React.FC = () => {
+  const onClick = () => {
+    window.location.href = "http://localhost:5000/auth/google";
+  };
 
-    const onClick = () => {
-        window.location.href = 'http://localhost:5000/auth/google'
-    }
-
-    return (
-        <div className='flex items-center justify-center w-full h-full'>
-            <Button icon={<GoogleIcon width={32} height={32} />} color={'outline'} className={'border-2'} id={'google-btn'} onClick={onClick}>
-                Sign in with Google
-            </Button>
-        </div>
-    )
-}
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <Button
+        icon={<GoogleIcon width={32} height={32} />}
+        color={"outline"}
+        className={"border-2"}
+        id={"google-btn"}
+        onClick={onClick}
+      >
+        Sign in with Google
+      </Button>
+    </div>
+  );
+};
