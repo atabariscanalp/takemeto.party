754428b4846a39a9ada7a885ee728c347d03be14
atabariscanalp <atabariscanalp@outlook.com>
2021-06-11 01:47:32 +0300

    -apollo config

--- paris/src/pages/home.tsx
+++ paris/src/pages/home.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from "react";
+import client from "../graphql/apollo-client";
+import { gql } from "@apollo/client";
 
-const Home = () => {
+const Home = ({ user }) => {
+  return (
+    <>
+      <h1>welcome to home bitch </h1>
+      <p>
+        {user.username} - {user.email}
+      </p>
+    </>
+  );
+};
 
-    return (
-        <h1>welcome to home bitch</h1>
-    )
+export async function getServerSideProps(context) {
+  const cookie = context.req.headers.cookie;
+  const { data } = await client.query({
+    query: gql`
+      query me {
+        me {
+          username
+          email
+        }
+      }
+    `,
+    context: {
+      headers: {
+        Cookie: cookie,
+      },
+    },
+  });
+
+  return {
+    props: {
+      user: data.me,
+    },
+  };
 }
- 
+
 export default Home;
